b4def21cce68efc07567877a0fca73599a03a2d2
pollirey <psmirnova9@yandex.ru>
2026-07-09 09:14:18 +0300

    Верстка автосалона

---
+++ index.js
@@ -0,0 +1,22 @@
+const carsData = [
+    {
+        name: "BMW M4 Competition",
+        subtitle: "Идеальный баланс скорости и стиля. BMW M4 Competition — 510 л.с. и драйв, созданный для покорения трасс и городских улиц.",
+        imageClass: "./assets/images/1.jpg",
+        brand: "BMW",
+        prices: [
+            {
+                period: "на 1 сутки",
+                price: "1450 $"
+            },
+            {
+                period: "на 1-3 суток",
+                price: "1300 $ /сут"
+            },
+            {
+                period: "на 3+ суток",
+                price: "1100 $ /сут"
+            }
+        ]
+    }
+]
